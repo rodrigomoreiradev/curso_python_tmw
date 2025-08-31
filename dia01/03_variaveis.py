d73b254dee04df19c8e693f90ccded5621ce7720
Rodrigo <rodrigojs20@gmail.com>
2025-08-31 13:04:02 -0300

    feat: aula sobre print, variaveis e operadores

---
+++ dia01/03_variaveis.py
@@ -0,0 +1,24 @@
+# %%
+nome = 'Rodrigo'
+print(nome)
+
+nome = 'Garrafa de Agua Levity'
+print(nome)
+
+# %%
+
+
+soma = 1 + 1
+
+print('1 + 1 = ', soma)
+# %%
+
+resultado = 10 - 5
+print(resultado)
+
+resultado = 10 * 2
+print(resultado)
+
+
+resultado = resultado + 10
+print(resultado)
