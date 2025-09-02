3ca4926128353d134e38411f13e029fede7126d7
Rodrigo <rodrigojs20@gmail.com>
2025-09-02 09:00:20 -0300

    feat: aula sobre for

---
+++ dia03/03_for.py
@@ -0,0 +1,15 @@
+# %%
+nome = 'Rodrigo Moreira'
+
+for letra in nome:
+    print(letra)
+    
+# %%
+
+numero = 2
+
+for i in range(1,101):
+    print(f'{i} x {numero} = {i * numero}')
+
+print('FIM!')
+# %%
