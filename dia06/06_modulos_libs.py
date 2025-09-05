a23ef856aa4fc2f4c02990057ba429ce455faa34
Rodrigo <rodrigojs20@gmail.com>
2025-09-05 16:52:11 -0300

    feat: aula modulos

---
+++ dia06/06_modulos_libs.py
@@ -0,0 +1,27 @@
+# %%
+import math
+from math import pi, e
+# from math import * <- não utilizar essa maneira
+
+import math as mh
+
+# %%
+
+math.sqrt(9)
+
+# %%
+math.pow(2,4)
+
+# %%
+math.e
+
+# %%
+math.pi
+
+# %%
+pi
+
+# %%
+e
+# %%
+mh.pi
